cfd9c3d478ba725b71d8d31c4d372f7b8650bb72
Elijah Tucker <etucker210@users.noreply.github.com>
2017-02-14 10:40:31 -0500

    add the Database definitions wich will be ignored on all other commits

---
+++ DBdefine.php
@@ -0,0 +1,7 @@
+<?php
+  $HOST = "YOUR-HOST"; //enter your hosts location here
+  $USER = "YOUR_USER_NAME"; //enter your DB user name here
+  $PASS = "YOUR_PASSWORD"; //enter your DB password here
+  $dbName = "YOUR_DB_NAME"; //enter your DB name here
+
+ ?>
